277d719f2ea436f67c7056541fbc49b389ad5aa2
Mark Lester Acak <mister6virgo@gmail.com>
2022-09-28 07:06:41 +0800

    The Second Version of the Mapty App

--- script.js
+++ script.js
@@ -10,3 +10,62 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+// To access the map and it's event to be global
+let map, mapEvent;
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (pos) {
+      const { latitude, longitude } = pos.coords;
+
+      const coords = [latitude, longitude];
+
+      map = L.map('map').setView(coords, 30);
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      map.on('click', function (mapE) {
+        mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
+      });
+    },
+    function () {
+      alert("Couldn't get your exact location.");
+    }
+  );
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  inputDistance.value =
+    inputDuration.value =
+    inputElevation.value =
+    inputCadence.value =
+      '';
+
+  const { lat, lng } = mapEvent.latlng;
+
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 150,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('This is where you will workout!')
+    .openPopup();
+});
+
+inputType.addEventListener('change', function () {
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+});
